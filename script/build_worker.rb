0272ed0281cce1e3fe31db3898e5223844c8fbd5
Hartog C. de Mik <hartog@organisedminds.com>
2013-01-12 16:24:16 +0100

    Start building

--- script/build_worker.rb
+++ script/build_worker.rb
@@ -4,10 +4,12 @@ EM.run {
   client.subscribe('/build/project') do |message|
     project = Project.find(message[:project])
     repo = project.repositories.find(message[:repo])
-    
+
     if repo.checkout(message[:commit])
       repo.lock do
-        
+        Thread.new {
+          Heidi2::Builder.build(repo, message[:commit])
+        }
       end
     end
   end
