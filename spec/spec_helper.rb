b5464e263196ea7916f421758b2555174c6021fe
Hartog C. de Mik <hartog@organisedminds.com>
2013-01-18 21:17:24 +0100

    Force tty

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,9 +9,10 @@ require 'rspec/autorun'
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
+  config.tty = true
   config.infer_base_class_for_anonymous_controllers = false
   config.order = "random"
-  
+
   require 'database_cleaner'
   config.before(:suite) do
     DatabaseCleaner.strategy = :truncation
@@ -21,8 +22,8 @@ RSpec.configure do |config|
   config.before(:each) do
     DatabaseCleaner.clean
   end
-  
+
   config.include Mongoid::Matchers
-  
+
   config.include Devise::TestHelpers, :type => :controller
 end
